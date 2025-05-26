39c8859e91efab9a8f8e1471b4bd6e4d44509abe
khaihanhtang <84257457+khaihanhtang@users.noreply.github.com>
2025-05-26 17:31:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from pytz.tzinfo import StaticTzInfo
 
 class Config:
     admins: set[str] = {"khaihanhtang", "bibi_tran", "ledung_jenny"}
-    # allowed_chat_ids: set[int] = {-1002228202437, -4273658267}  # official
-    # default_chat_id = -1002228202437  # official
-    allowed_chat_ids: set[int] = {-4273658267}  # test
-    default_chat_id = -4273658267  # test
+    allowed_chat_ids: set[int] = {-1002228202437, -4273658267}  # official
+    default_chat_id = -1002228202437  # official
+    # allowed_chat_ids: set[int] = {-4273658267}  # test
+    # default_chat_id = -4273658267  # test
     time_zone: StaticTzInfo = pytz.timezone("Asia/Singapore")
     input_time_format: str = "%H:%M:%S %d/%m/%Y"
     output_time_format: str = "%H:%M:%S %A %d-%B-%Y (%Z)"
